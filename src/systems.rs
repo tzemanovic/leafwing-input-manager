6501eca63753f31d37c7b0b6480a10d224b571e2
Tomáš Zemanovič <tzemanovic@gmail.com>
2023-08-01 08:04:27 +0100

    fixup! dbg gamepad axes

--- src/systems.rs
+++ src/systems.rs
@@ -99,7 +99,7 @@ pub fn update_action_state<A: Actionlike>(
     let gamepad_button_axes = gamepad_button_axes.into_inner();
     let gamepad_axes = gamepad_axes.into_inner();
     for device in gamepad_axes.devices() {
-        let pos = gamepad_axes.get(device);
+        let pos = gamepad_axes.get(*device);
         if let Some(pos) = pos {
             if pos != 0.0 {
                 dbg!(device.axis_type, pos);
